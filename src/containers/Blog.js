1fe87e2dcbfb772cdab7969c7f942c93ac864318
Daniel Mauno Pettersson <daniel@dooer.com>
2018-02-22 09:03:01 +0100

    🔥 Cleanup

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,19 +1,7 @@
 
 import React from 'react'
-import { withRouteData, Link } from 'react-static'
-//
+import { withRouteData } from 'react-static'
 
-export default withRouteData(({ posts }) => (
-  <div>
-    <h1>It's blog time.</h1>
-    <br />
-    All Posts:
-    <ul>
-      {posts.map(post => (
-        <li key={post.id}>
-          <Link to={`/blog/post/${post.id}/`}>{post.title}</Link>
-        </li>
-      ))}
-    </ul>
-  </div>
+export default withRouteData(() => (
+  <div />
 ))
