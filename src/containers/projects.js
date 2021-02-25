851094f6c2552734b2456699943e8663cdbfcc84
Daniel Mauno Pettersson <daniel@mauno.io>
2021-02-25 12:10:54 +0100

    Add memmo

--- src/containers/projects.js
+++ src/containers/projects.js
@@ -6,19 +6,36 @@ import ahlensLogo from '../img/ahlenslogo.svg'
 import billoLogo from '../img/billogramlogo.svg'
 import googleLogo from '../img/googlelogo.svg'
 import uppconLogo from '../img/uppconlogo.svg'
+import memmoLogo from '../img/memmologo.svg'
 import bidsterLogo from '../img/bidsterlogo.png'
 import sjLogo from '../img/sjlogo.svg'
+
+const memmo = {
+  title: 'Memmo ',
+  slug: 'memmo',
+  when: 'Nov 2019 - now',
+  subtitle: 'CTO',
+  logotype: memmoLogo,
+  involved: '',
+  description:
+    'Platform connecting talent with their following. Offering personalized video greetings. Node / GraphQL / React / React Native / Google Cloud.',
+}
 
 const freelance = {
   title: 'Freelance ',
   slug: 'freelance',
-  when: 'May 2019 - now',
+  when: 'May 2019 - Nov 2019',
   subtitle: 'CTO / Fullstack developer',
   logotype: () => (
-    <div style={{ fontSize: 50 }}><span role="img" aria-label="Coffee emoji">☕</span></div>
+    <div style={{ fontSize: 50 }}>
+      <span role="img" aria-label="Coffee emoji">
+        ☕
+      </span>
+    </div>
   ),
   involved: '',
-  description: 'Helping both startups and established businesses with strategy, hiring, architecture and development. Feel free to hire me :)',
+  description:
+    'Helping both startups and established businesses with strategy, hiring, architecture and development.',
 }
 
 const alchemy = {
@@ -28,7 +45,8 @@ const alchemy = {
   subtitle: 'Head of Technology & Products',
   logotype: alchemyLogo,
   involved: '',
-  description: 'Product & service design studio. I hold Tech Director role in most projects and are in charge of building the tech team. Working with clients such as DHL, Musikhjälpen, Vattenfall, Volkswagen, Klarna.',
+  description:
+    'Product & service design studio. I held the Tech Director role in most projects and was in charge of building the tech team. Working with clients such as DHL, Musikhjälpen, Vattenfall, Volkswagen, Klarna.',
 }
 
 const dooer = {
@@ -38,7 +56,8 @@ const dooer = {
   subtitle: 'Head of Product & Development',
   logotype: dooerLogo,
   involved: '',
-  description: 'Fintech startup that acquired Agigen in 2016. Managed a team of about 30 developers, designers and team leads accross three locations. The project included microservices, GraphQL and machine learning.',
+  description:
+    'Fintech startup that acquired Agigen in 2016. Managed a team of about 30 developers, designers and team leads accross three locations. The project included microservices, GraphQL and machine learning.',
 }
 
 const agigen = {
@@ -48,7 +67,8 @@ const agigen = {
   subtitle: 'CEO & Founder',
   logotype: agigenLogo,
   involved: '',
-  description: 'Before joining Dooer I ran a digital production agency focused on high profile marketing campaigns and product development. We were fifteen employees when acquired by Dooer.',
+  description:
+    'My first own company. A digital production agency focused on high profile marketing campaigns and product development. We were fifteen employees when acquired by Dooer.',
 }
 
 const billogram = {
@@ -58,7 +78,8 @@ const billogram = {
   subtitle: 'CTO',
   logotype: billoLogo,
   involved: '',
-  description: 'The most successful product development project we ran at Agigen was Billogram. Billogram is an invoicing service focused on the customer experience. I worked as their CTO for the first four years, while also providing a team of consultants',
+  description:
+    'The most successful product development project we ran at Agigen was Billogram. Billogram is an invoicing service focused on the customer experience. I worked as their CTO for the first four years, while also providing a team of consultants',
 }
 
 const ahlens = {
@@ -68,7 +89,8 @@ const ahlens = {
   subtitle: 'Frontend Architect',
   logotype: ahlensLogo,
   involved: '',
-  description: 'Project manager & technical architeture for the frontend for Åhlens.se, Swedens largest retail chain, as they moved into omnichannel commerce.',
+  description:
+    'Project manager & technical architeture for the frontend for Åhlens.se, Swedens largest retail chain, as they moved into omnichannel commerce.',
 }
 
 const build = {
@@ -78,7 +100,8 @@ const build = {
   subtitle: 'LEGO & Google',
   logotype: googleLogo,
   involved: '',
-  description: 'Marketing campaign showcasing 3D capabilities of Google Chrome by building LEGO in the browser. I lead the Agigen team who built parts of the experience together with North Kingdom.',
+  description:
+    'Marketing campaign showcasing 3D capabilities of Google Chrome by building LEGO in the browser. I lead the Agigen team who built parts of the experience together with North Kingdom.',
 }
 
 const sj = {
@@ -88,7 +111,8 @@ const sj = {
   subtitle: 'Pilot project for their new site',
   logotype: sjLogo,
   involved: '',
-  description: 'I lead the techincal part together with the agency POND for creating the new version of sj.se, the site for the public train company in Sweden. A successful project that turned into their current solution.',
+  description:
+    'I lead the techincal part together with the agency POND for creating the new version of sj.se, the site for the public train company in Sweden. A successful project that turned into their current solution.',
 }
 
 const bidster = {
@@ -98,7 +122,8 @@ const bidster = {
   subtitle: 'Fullstack Developer',
   logotype: bidsterLogo,
   involved: '',
-  description: 'My first full-time job in tech. Developer for the startup Bidster, that did lowest-unique-bid auctions.',
+  description:
+    'My first full-time job in tech. Developer for the startup Bidster, a platform for lowest-unique-bid auctions.',
 }
 
 const uppcon = {
@@ -108,10 +133,12 @@ const uppcon = {
   subtitle: 'Founder & Head Organizer',
   logotype: uppconLogo,
   involved: '',
-  description: 'Scandinavias largest event for youths interested in Japanese popular culture. A non-profit I founded which attracted around 5000 visitors during four days at it\'s peak',
+  description:
+    'Scandinavias largest event for youths interested in Japanese popular culture. A non-profit I founded which attracted around 5000 visitors during four days at its peak',
 }
 
 export default [
+  memmo,
   freelance,
   alchemy,
   dooer,
